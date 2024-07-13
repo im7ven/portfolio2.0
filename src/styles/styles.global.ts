0ac75fab7d51a69acee766c73f71ba5b109273c2
Justin <alexanderrr416@gmail.com>
2024-07-13 15:25:31 -0400

    chore: Set up styled-components

---
+++ src/styles/styles.global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+html {
+    font-size: 62.5%;
+    scroll-behavior:smooth;
+ }
+ 
+ body {
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+ }
+
+
+`;
