18f51baf046cba49d031449d18072906641c3650
Justin <alexanderrr416@gmail.com>
2024-07-13 21:35:23 -0400

    style: Add reusable styles

--- src/styles.global.ts
+++ src/styles.global.ts
@@ -1,6 +1,16 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+:root{
+--primary-color: #8244FF;
+--secondary-color: #16F8B6;
+--accent-color: #F926AE;
+--background-primary: #0D1224;
+--body-text: #b8c1ec;
+--border-radius: 8px;
+
+}
+
 html {
     font-size: 62.5%;
     scroll-behavior:smooth;
@@ -12,5 +22,24 @@ html {
     box-sizing: border-box;
  }
 
+`;
 
+// Typography
+
+const h1 = css`
+  font-size: 3rem;
+
+  @media screen and (min-width: 768px) {
+    font-size: 5rem;
+  }
+`;
+
+const bodyText = styled.p`
+  font-size: 1.8rem;
+  color: var(--body-text);
+  line-height: 30px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 2.1rem;
+  }
 `;
