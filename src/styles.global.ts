58fb3f98de72d336e57a2fabfe812d6833c30559
Justin <alexanderrr416@gmail.com>
2024-07-14 15:58:01 -0400

    style: add some reusable styles

--- src/styles.global.ts
+++ src/styles.global.ts
@@ -8,7 +8,13 @@ export const GlobalStyles = createGlobalStyle`
 --background-primary: #0D1224;
 --body-text: #b8c1ec;
 --border-radius: 8px;
+--primary-font:  "Gabarito", sans-serif;
 
+}
+
+* {
+padding: 0;
+margin: 0;
 }
 
 html {
@@ -17,29 +23,53 @@ html {
  }
  
  body {
-    margin:0;
-    padding: 0;
+ font-family: var(--primary-font);
     box-sizing: border-box;
+    background-color: var(--background-primary);
  }
 
 `;
 
 // Typography
 
-const h1 = css`
-  font-size: 3rem;
-
+export const h1 = css`
   @media screen and (min-width: 768px) {
     font-size: 5rem;
   }
 `;
 
-const bodyText = styled.p`
+export const bodyText = styled.p`
   font-size: 1.8rem;
   color: var(--body-text);
   line-height: 30px;
 
   @media screen and (min-width: 768px) {
     font-size: 2.1rem;
+  }
+`;
+
+// Utils
+
+export const sectionBlock = css`
+  border-bottom: 1px solid #cecece;
+  min-height: 100vh;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 45%;
+    right: 45%;
+    box-shadow: #7a55ff 0px 0px 20rem 6rem;
+  }
+`;
+
+export const grid1x2 = css`
+  display: grid;
+  justify-items: center;
+  align-items: center;
+
+  @media screen and (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
   }
 `;
