604f5462d09c7f84f419d84dd63175c16e6b79de
Justin <alexanderrr416@gmail.com>
2024-07-16 21:58:35 -0400

    feat: build animating text component

---
+++ src/components/AnimatedText.tsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion";
+
+const AnimatedText = ({ text }: { text: string }) => {
+  const defaultAnimations = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+    },
+  };
+
+  return (
+    <motion.span
+      initial="hidden"
+      animate="visible"
+      transition={{ staggerChildren: 0.05 }}
+    >
+      {text.split("").map((char, index) => (
+        <motion.span variants={defaultAnimations} key={index}>
+          {char}
+        </motion.span>
+      ))}
+    </motion.span>
+  );
+};
+
+export default AnimatedText;
