b53bfd20d6f01df67e3c9e03c70ef3f40f6c7a47
Justin <alexanderrr416@gmail.com>
2024-07-18 12:13:09 -0400

    feat: build a reveal animation component

---
+++ src/components/RevealAnimation.tsx
@@ -0,0 +1,78 @@
+import { motion, useInView, useAnimation, easeIn } from "framer-motion";
+import { ReactNode, useRef, useEffect } from "react";
+import styled from "styled-components";
+
+interface Props {
+  children: ReactNode;
+  width?: "fit-content" | "100%";
+  overflow?: "hidden" | "visible";
+}
+
+const RevealWrapper = styled(motion.div)<Props>`
+  position: relative;
+  overflow: ${(props) => props.overflow};
+  width: ${(props) => props.width};
+
+  &.avatar {
+    overflow: visible;
+  }
+`;
+
+const Slider = styled(motion.div)`
+  position: absolute;
+  top: 4px;
+  bottom: 4px;
+  left: 0;
+  right: 0;
+  z-index: 7;
+  background: linear-gradient(
+    165deg,
+    rgba(13, 18, 36, 1) 0%,
+    rgba(37, 44, 68, 1) 100%
+  );
+`;
+
+const RevealAnimation = ({
+  children,
+  width = "fit-content",
+  overflow = "hidden",
+}: Props) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      slideControls.start("visible");
+    }
+  }, [isInView]);
+  return (
+    <RevealWrapper ref={ref} width={width} overflow={overflow}>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
+      <Slider
+        variants={{
+          hidden: { bottom: 0 },
+          visible: { bottom: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        transition={{ duration: 0.5, ease: easeIn }}
+      />
+    </RevealWrapper>
+  );
+};
+
+export default RevealAnimation;
