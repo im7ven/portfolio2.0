47a8763d3a0aef92fafe033a1d68a37d41fd1cd9
Justin <alexanderrr416@gmail.com>
2024-07-18 12:26:22 -0400

    refactor: change the layout and add social icons to nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,46 @@
 import styled from "styled-components";
 import { ListItem } from "../styles.global";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { TbBrandGithubFilled } from "react-icons/tb";
+import { IoIosMail } from "react-icons/io";
 
 const NavbarWrapper = styled.nav`
-  display: flex;
-  justify-content: space-between;
+  display: none;
+  // justify-content: center;
   align-items: center;
   position: absolute;
   width: 100%;
-  margin: 2rem auto 0;
   top: 0;
-  left: 50%;
-  transform: translateX(-50%);
+  box-sizing: border-box;
   z-index: 7;
-  box-shadow: 0 0px 5px 10px rgba(0, 0, 0, 0.1);
-  background: inherit;
+  box-shadow: 0px 0px 0px 0.1px #cccdcd;
   padding: 1rem;
 
   @media screen and (min-width: 768px) {
-    padding: 3rem 3.5rem;
-    margin: 4rem auto 0;
+    display: flex;
+    padding: 2rem 3.5rem;
+    margin: 0rem auto 0;
+    justify-content: center;
   }
 `;
 
 const NavList = styled.ul`
-  width: 100%;
   display: flex;
   justify-content: center;
   gap: 3rem;
   list-style: none;
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  position: absolute;
+  right: 1rem;
+
+  @media screen and (min-width: 768px) {
+    right: 3.5rem;
+  }
 `;
 
 const Navbar = () => {
@@ -38,6 +51,11 @@ const Navbar = () => {
         <ListItem>Skills</ListItem>
         <ListItem>Contact</ListItem>
       </NavList>
+      <IconWrapper>
+        <FaLinkedinIn size="28px" color="#fff" />
+        <TbBrandGithubFilled size="28px" color="#fff" />
+        <IoIosMail size="28px" color="#fff" />
+      </IconWrapper>
     </NavbarWrapper>
   );
 };
