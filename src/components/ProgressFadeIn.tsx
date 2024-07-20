7c0097c9d458776a13b94b994fabdd90cd3525c3
Justin <alexanderrr416@gmail.com>
2024-07-20 00:09:58 -0400

    refactor: change translate value for larger screens

--- src/components/ProgressFadeIn.tsx
+++ src/components/ProgressFadeIn.tsx
@@ -1,11 +1,10 @@
-import { motion } from "framer-motion";
-import { ReactNode } from "react";
+import { easeIn, easeInOut, motion } from "framer-motion";
+import { ReactNode, useEffect, useState } from "react";
 
 interface Props {
   children: ReactNode;
   index: number;
   initial?: string;
-  animate?: string;
 }
 
 const fadeInAnimationVariants = {
@@ -17,16 +16,51 @@ const fadeInAnimationVariants = {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 0.4,
+      duration: 0.3,
+      delay: 0.4 + index * 0.1,
+    },
+  }),
+};
+
+const fadeInAnimationVariantsLargeScreen = {
+  initial: {
+    opacity: 0,
+    x: -30,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.3,
       delay: 0.4 + index * 0.1,
     },
   }),
 };
 
 const ProgressFadeIn = ({ children, index, initial = "initial" }: Props) => {
+  const [isLargeScreen, setIsLargeScreen] = useState(
+    window.matchMedia("(min-width: 1250px)").matches
+  );
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 1250px)");
+    const handleMediaChange = (e: MediaQueryListEvent) =>
+      setIsLargeScreen(e.matches);
+
+    mediaQuery.addEventListener("change", handleMediaChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaChange);
+    };
+  }, []);
+
+  const variants = isLargeScreen
+    ? fadeInAnimationVariantsLargeScreen
+    : fadeInAnimationVariants;
+
   return (
     <motion.div
-      variants={fadeInAnimationVariants}
+      variants={variants}
       initial={initial}
       animate="animate"
       custom={index}
