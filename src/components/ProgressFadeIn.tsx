761f3509d81898c82a60fde0f1ab492a71428514
Justin <alexanderrr416@gmail.com>
2024-07-20 21:20:12 -0400

    refactor: pass prop to delay animation

--- src/components/ProgressFadeIn.tsx
+++ src/components/ProgressFadeIn.tsx
@@ -1,10 +1,11 @@
-import { easeIn, easeInOut, motion } from "framer-motion";
+import { delay, easeIn, easeInOut, motion } from "framer-motion";
 import { ReactNode, useEffect, useState } from "react";
 
 interface Props {
   children: ReactNode;
   index: number;
   initial?: string;
+  delay?: number;
 }
 
 const fadeInAnimationVariants = {
@@ -22,22 +23,12 @@ const fadeInAnimationVariants = {
   }),
 };
 
-const fadeInAnimationVariantsLargeScreen = {
-  initial: {
-    opacity: 0,
-    x: -30,
-  },
-  animate: (index: number) => ({
-    opacity: 1,
-    x: 0,
-    transition: {
-      duration: 0.3,
-      delay: 0.4 + index * 0.15,
-    },
-  }),
-};
-
-const ProgressFadeIn = ({ children, index, initial = "initial" }: Props) => {
+const ProgressFadeIn = ({
+  children,
+  index,
+  initial = "initial",
+  delay = 0.4,
+}: Props) => {
   const [isLargeScreen, setIsLargeScreen] = useState(
     window.matchMedia("(min-width: 1250px)").matches
   );
@@ -53,6 +44,21 @@ const ProgressFadeIn = ({ children, index, initial = "initial" }: Props) => {
       mediaQuery.removeEventListener("change", handleMediaChange);
     };
   }, []);
+
+  const fadeInAnimationVariantsLargeScreen = {
+    initial: {
+      opacity: 0,
+      x: -30,
+    },
+    animate: (index: number) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.3,
+        delay: delay + index * 0.15,
+      },
+    }),
+  };
 
   const variants = isLargeScreen
     ? fadeInAnimationVariantsLargeScreen
