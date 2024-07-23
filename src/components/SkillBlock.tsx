65d954e614f9fc2af1f3539892db825a5eb7e795
Justin <alexanderrr416@gmail.com>
2024-07-23 19:25:19 -0400

    feat: build skill block section

--- src/components/SkillBlock.tsx
+++ src/components/SkillBlock.tsx
@@ -3,9 +3,15 @@ import { BlockHeading, sectionBlock } from "../styles.global";
 import AnimatedText from "./AnimatedText";
 import skills from "./skillsData";
 import TechCard from "./TechCard";
+import SoftSkillFeature from "./SoftSkillFeature";
+import communicationIcon from "../images/communication.png";
+import creativityIcon from "../images/creativity.png";
+import collaboration from "../images/collaboration.png";
+import FadeUp from "./FadeUp";
 
 const SkillContainer = styled.section`
   ${sectionBlock}
+  padding-bottom: 6rem;
 `;
 
 const SkillGrid = styled.div`
@@ -18,17 +24,56 @@ const SkillGrid = styled.div`
   padding: 0 2.4rem;
 `;
 
+const SoftSkillWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+  margin-top: 8rem;
+  padding: 0 2.4rem;
+`;
+
 const SkillBlock = () => {
   return (
     <SkillContainer>
       <BlockHeading>
         <AnimatedText text="Some of my skills"></AnimatedText>
       </BlockHeading>
-      <SkillGrid>
-        {skills.map((skill) => (
-          <TechCard {...skill} />
-        ))}
-      </SkillGrid>
+      <FadeUp>
+        <SkillGrid>
+          {skills.map((skill) => (
+            <TechCard {...skill} />
+          ))}
+        </SkillGrid>
+      </FadeUp>
+      <SoftSkillWrapper>
+        <FadeUp>
+          <SoftSkillFeature icon={communicationIcon} heading="Communication">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+            aspernatur aliquid illo voluptatum sed quo dolore consequuntur
+            cupiditate earum, nulla maxime a dolorem laboriosam assumenda
+            aperiam beatae alias mollitia rerum esse officia asperiores
+            doloremque? Nostrum minus rerum molestias quo eum!
+          </SoftSkillFeature>
+        </FadeUp>
+        <FadeUp>
+          <SoftSkillFeature icon={creativityIcon} heading="Communication">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+            aspernatur aliquid illo voluptatum sed quo dolore consequuntur
+            cupiditate earum, nulla maxime a dolorem laboriosam assumenda
+            aperiam beatae alias mollitia rerum esse officia asperiores
+            doloremque? Nostrum minus rerum molestias quo eum!
+          </SoftSkillFeature>
+        </FadeUp>
+        <FadeUp>
+          <SoftSkillFeature icon={collaboration} heading="Communication">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+            aspernatur aliquid illo voluptatum sed quo dolore consequuntur
+            cupiditate earum, nulla maxime a dolorem laboriosam assumenda
+            aperiam beatae alias mollitia rerum esse officia asperiores
+            doloremque? Nostrum minus rerum molestias quo eum!
+          </SoftSkillFeature>
+        </FadeUp>
+      </SoftSkillWrapper>
     </SkillContainer>
   );
 };
