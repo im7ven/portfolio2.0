8a1006b49494d84eb8e106adf8a8ebb56c3ad7fb
Justin <alexanderrr416@gmail.com>
2024-07-24 18:11:02 -0400

    chore: apply title and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import ContactBlock from "./components/ContactBlock";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
+import ProjectBlock from "./components/ProjectBlock";
+import SkillBlock from "./components/SkillBlock";
 import { GlobalStyles } from "./styles.global";
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
+      <Menu />
       <Navbar />
       <Hero />
+      <ProjectBlock />
+      <SkillBlock />
+      <ContactBlock />
+      <Footer />
     </>
   );
 };
